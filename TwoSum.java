9980157c9ce81c71a744783f273d396dda792d92
Yujin <95773720+yujin991110@users.noreply.github.com>
2026-05-11 15:33:14 -0500

    Update TwoSum.java

--- TwoSum.java
+++ TwoSum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
-            for(int j = 1; j < nums.length; j++) {
+            for(int j = i+1; j < nums.length; j++) {
                 if(nums[i] + nums [j] == target) {
                     return new int[]{i,j};
                 }
